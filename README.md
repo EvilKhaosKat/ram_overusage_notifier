d9ac7498e5dd54e15137c88fda6a50156c746dcf
Roman Garanin <evilkhaoskat@gmail.com>
2020-01-08 22:24:19 +0300

    feat: use actual psutil version

--- README.md
+++ README.md
@@ -2,13 +2,12 @@ ram_overusage_notifier
 ======================
 
 Script for ram usage tracking, in case of strange sudden ram overusage makes an alarm using 'notify-send'.<br>
-Checks if there any processes those use more than DEFAULT_PROCESS_MEMORY_LIMIT_MB<br>
+Checks if there are any processes those use more than `DEFAULT_PROCESS_MEMORY_LIMIT_MB`<br>
 For using:<br>
-1. 'pip install -r requirements.txt' to get needful libraries (psutil at the moment)<br>
-2. call 'python main.py'<br>
+1. `pip install -r requirements.txt` to get needful libraries (`psutil` at the moment)<br>
+2. call `python main.py`<br>
 3. profit =)
 
 ### Prerequisites:
-* git
-* torsocks
-* notify-send
+* linux and `notify-send` installed
+* python 3
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import psutil
 import time
 import os
-#import sys
+
 
 class Main:
-    DEFAULT_ICON = '/usr/share/icons/gnome/32x32/status/software-update-urgent.png'
-    DEFAULT_EXPIRE_TIME = 7000
+    DEFAULT_EXPIRE_TIME_MS = 7000
     DEFAULT_PROCESS_MEMORY_LIMIT_MB = 2048
     DEFAULT_SLEEP_PERIOD_SEC = 5
     EXCLUDED_PROCESS_NAMES = []
+
+    @staticmethod
+    def check_ram_overusage():
+        previous_max = None
+
+        while True:
+            processes = list(psutil.process_iter())
+            processes = list(filter(lambda process: process.name() not in Main.EXCLUDED_PROCESS_NAMES, processes))
+
+            if len(processes) > 0:
+                max_memory_process = max(processes, key=Main.get_process_used_memory_mb)
+
+                used_memory_mb = Main.get_process_used_memory_mb(max_memory_process)
+                if used_memory_mb > Main.DEFAULT_PROCESS_MEMORY_LIMIT_MB:
+                    if not previous_max or previous_max.pid != max_memory_process.pid:
+                        Main.send_notify("Process {process_name} uses {used_memory:.2f} mb of RAM"
+                                         .format(process_name=max_memory_process.name(), used_memory=used_memory_mb))
+
+                    previous_max = max_memory_process
+
+            time.sleep(Main.DEFAULT_SLEEP_PERIOD_SEC)
 
     @staticmethod
     def get_process_used_memory_mb(process):
@@ -17,29 +37,9 @@ class Main:
 
     @staticmethod
     def send_notify(message):
-        os.system("notify-send --urgency=critical -i {icon} -t {expire_time} 'ram overusage notify' '{message}'"
-                  .format(icon=Main.DEFAULT_ICON, expire_time=Main.DEFAULT_EXPIRE_TIME, message=message))
-
-    @staticmethod
-    def check_ram_overusage():
-        previous_max = None
-
-        while True:
-            processes = psutil.get_process_list()
-            processes = list(filter(lambda process: process.name() not in Main.EXCLUDED_PROCESS_NAMES, processes))
-
-            if len(processes) > 0:
-                max_memory_used_process = max(processes, key=Main.get_process_used_memory_mb)
-
-                used_memory_mb = Main.get_process_used_memory_mb(max_memory_used_process)
-                if used_memory_mb > Main.DEFAULT_PROCESS_MEMORY_LIMIT_MB:
-                    if not previous_max or previous_max.pid != max_memory_used_process.pid:
-                        Main.send_notify("Process {process_name} uses {used_memory:.2f} mb of RAM"
-                                         .format(process_name=max_memory_used_process.name(), used_memory=used_memory_mb))
-
-                    previous_max = max_memory_used_process
-
-            time.sleep(Main.DEFAULT_SLEEP_PERIOD_SEC)
+        # TODO os independent impl required
+        os.system("notify-send --urgency=critical -t {expire_time} 'ram overusage notify' '{message}'"
+                  .format(expire_time=Main.DEFAULT_EXPIRE_TIME_MS, message=message))
 
 
 Main().check_ram_overusage()
